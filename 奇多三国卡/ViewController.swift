9399505c4c1a5ed5366c66c9c4835ca7d1ca111b
cyp0911 <cyp0911@gmail.com>
2016-01-23 22:54:41 -0400

    Initial Commit

---
+++ 奇多三国卡/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  奇多三国卡
+//
+//  Created by 陈音澎 on 1/23/16.
+//  Copyright © 2016 Clark Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
